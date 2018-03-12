3f00dafea3df4ceb355e86722266084fcd0cce26
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-12 20:15:10 +0900

    タイポを修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Mail Func.
-  cconfig.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
